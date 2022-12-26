998749bc4290221d72676707af9f9dfdafb05868
JuanBaezaB <51477488+JuanBaezaB@users.noreply.github.com>
2022-12-26 16:30:49 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements  OnInit {
+  theme: any;
+
+  constructor(
+    private themeService: ThemeService
+  ) {}
+
+  async ngOnInit(){
+    this.theme= this.themeService.getTheme();
+    console.log(this.theme);
+    if(this.theme == "light" || this.theme==""){
+      this.themeService.enableLight();
+    }else{
+      this.themeService.enableDark();
+    }
+  }
 }
